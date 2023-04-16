1f23ee2161bbc7e4e19e07a2e54f2269c569d09b
zed-chi <zed-chi@mail.ru>
2023-04-17 00:03:43 +0300

    added teaching script

---
+++ teach_dialog.py
@@ -0,0 +1,15 @@
+import json
+from dialog import create_intent
+from environs import Env
+
+env = Env()
+env.read_env()
+
+with open("./questions.json", "r", encoding="utf-8") as file:
+    questions: dict = json.load(file)
+
+
+for intent in questions.keys():
+    phrases = questions[intent]["questions"]
+    answer = questions[intent]["answer"]
+    create_intent(env.str("PROJECT_ID"), intent, phrases, [answer])
