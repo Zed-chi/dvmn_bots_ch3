3740040cbef789f43e3c6d8e94040675e44abd3c
zed-chi <zed-chi@mail.ru>
2023-04-16 23:31:11 +0300

    Added return to dialog func

--- dialog.py
+++ dialog.py
@@ -1,44 +1,20 @@
-from dotenv import load_dotenv
-load_dotenv()
-import os
+from google.cloud import dialogflow
+
 
 def detect_intent_texts(project_id, session_id, text, language_code):
-    """Returns the result of detect intent with texts as inputs.
-
+    """
+    Returns the result of detect intent with texts as inputs.
     Using the same `session_id` between requests allows continuation
-    of the conversation."""
-    import dialogflow_v2 as dialogflow
+    of the conversation.
+    """
+
     session_client = dialogflow.SessionsClient()
-
     session = session_client.session_path(project_id, session_id)
-    print('Session path: {}\n'.format(session))
 
-    text_input = dialogflow.types.TextInput(
-        text=text, language_code=language_code)
-
-    query_input = dialogflow.types.QueryInput(text=text_input)
+    text_input = dialogflow.TextInput(text=text, language_code=language_code)
+    query_input = dialogflow.QueryInput(text=text_input)
 
     response = session_client.detect_intent(
-        session=session, query_input=query_input)
-
-    print('=' * 20)
-    print('Query text: {}'.format(response.query_result.query_text))
-    print('Detected intent: {} (confidence: {})\n'.format(
-        response.query_result.intent.display_name,
-        response.query_result.intent_detection_confidence))
-    print('Fulfillment text: {}\n'.format(
-        response.query_result.fulfillment_text))
+        request={"session": session, "query_input": query_input}
+    )
     return response.query_result.fulfillment_text
-
-def implicit():
-    from google.cloud import storage
-
-    # If you don't specify credentials when constructing the client, the
-    # client library will look for credentials in the environment.
-    storage_client = storage.Client()
-
-    # Make an authenticated API request
-    buckets = list(storage_client.list_buckets())
-    print(buckets)
-
-#implicit()
