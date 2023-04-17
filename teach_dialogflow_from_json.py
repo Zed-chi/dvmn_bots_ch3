aa2633606b1b6dcca4805d920ee1a31d2f07f142
zed-chi <zed-chi@mail.ru>
2023-04-17 22:21:07 +0300

    renamed file

---
+++ teach_dialogflow_from_json.py
@@ -0,0 +1,17 @@
+import json
+
+from environs import Env
+
+from dialog import create_intent
+
+env = Env()
+env.read_env()
+
+if __name__ == "__main__":
+    with open("./questions.json", "r", encoding="utf-8") as file:
+        questions: dict = json.load(file)
+
+    for intent in questions.keys():
+        phrases = questions[intent]["questions"]
+        answer = questions[intent]["answer"]
+        create_intent(env.str("PROJECT_ID"), intent, phrases, [answer])
