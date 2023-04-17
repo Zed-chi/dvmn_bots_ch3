aaa33bbdee273fd8318e10988ed9678b5a8b1b8f
zed-chi <zed-chi@mail.ru>
2023-04-17 20:14:16 +0300

    added dialogflow answer func

--- vk_bot.py
+++ vk_bot.py
@@ -1,14 +1,16 @@
-import vk_api
-from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
-from environs import Env
 import random
 
+import vk_api
+from environs import Env
+from vk_api.bot_longpoll import VkBotEventType, VkBotLongPoll
+
+from dialog import detect_intent_texts
 
 env = Env()
 env.read_env("./.env")
 
 
-def echo(event, vk_api):
+def echo_answer(event, vk_api):
     print(f"event.message = {event.message}")
     user_id = event.message["from_id"]
     message = event.message["text"]
@@ -16,6 +18,17 @@ def echo(event, vk_api):
     vk_api.messages.send(
         user_id=user_id,
         message=message,
+        random_id=random.randint(1, 1000),
+    )
+
+
+def answer_from_dialogflow(event, vk_api):
+    user_id = event.message["from_id"]
+    message = event.message["text"]
+    answer = detect_intent_texts(env.str("PROJECT_ID"), user_id, message, "ru")
+    vk_api.messages.send(
+        user_id=user_id,
+        message=answer,
         random_id=random.randint(1, 1000),
     )
 
@@ -28,7 +41,7 @@ def main():
     for event in longpoll.listen():
         if event.type == VkBotEventType.MESSAGE_NEW:
             print("Новое сообщение:")
-            echo(event, api)
+            answer_from_dialogflow(event, api)
         elif event.type == VkBotEventType.MESSAGE_REPLY:
             print("Новое сообщение:")
             print("От меня для: ", end="")
