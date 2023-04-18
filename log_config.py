39526548f86e554e81feae88d6db72eb2f716105
zed-chi <zed-chi@mail.ru>
2023-04-18 09:58:21 +0300

    moved log logic to separate file

---
+++ log_config.py
@@ -0,0 +1,41 @@
+import logging
+from environs import Env
+from telegram import Bot
+
+env = Env()
+
+FORMAT = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+
+
+def get_logger(name):
+    logger = logging.getLogger(name)
+    logger.setLevel(getattr(logging, env.str("LOG_LEVEL", "WARNING")))
+    return logger
+
+
+class TelegramLogsHandler(logging.Handler):
+    def __init__(self, tg_bot, chat_id):
+        super().__init__()
+        self.chat_id = chat_id
+        self.tg_bot = tg_bot
+
+    def emit(self, record):
+        log_entry = self.format(record)
+        self.tg_bot.send_message(chat_id=self.chat_id, text=log_entry)
+
+
+def select_handler_by_env(
+    logger: logging.Logger,
+):
+    """options - FILE/TG/CONSOLE"""
+    log_env = env.str("LOGGER")
+    if log_env == "FILE":
+        handler = logging.FileHandler(env.str("LOG_PATH"))
+
+    elif log_env == "TG":
+        bot = Bot(token=env.str("TG_BOT_TOKEN"))
+        handler = TelegramLogsHandler(bot, env.str("TG_ADMIN_CHAT_ID"))
+    else:
+        handler = logging.StreamHandler()
+    handler.setFormatter(FORMAT)
+    logger.addHandler(handler)
