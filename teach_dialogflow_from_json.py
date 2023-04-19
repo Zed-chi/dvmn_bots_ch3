74a9f13111e7f7227edc8ce4d2fe13c9724f6303
zed-chi <zed-chi@mail.ru>
2023-04-19 20:05:52 +0300

    added path arg option

--- teach_dialogflow_from_json.py
+++ teach_dialogflow_from_json.py
@@ -1,16 +1,37 @@
 import json
-
+import argparse
 from dialog import create_intent
 from environs import Env
 
-env = Env()
-env.read_env()
+ENV = Env()
+ENV.read_env()
 
-if __name__ == "__main__":
+def get_args():
+    parser = argparse.ArgumentParser(description="DialogFlow ")    
+    parser.add_argument(
+        "-p", dest="path", default="./questions.json", 
+        type=str, description="Path to json file with questions"
+    )
+    args = parser.parse_args()
+
+def load_questions(filepath):
     with open("./questions.json", "r", encoding="utf-8") as file:
         questions: dict = json.load(file)
+        return questions
 
-        for intent in questions.keys():
-            phrases = questions[intent]["questions"]
-            answer = questions[intent]["answer"]
-            create_intent(env.str("PROJECT_ID"), intent, phrases, [answer])
+
+def teach(questions):
+    for intent in questions.keys():
+        phrases = questions[intent]["questions"]
+        answer = questions[intent]["answer"]
+        create_intent(ENV.str("PROJECT_ID"), intent, phrases, [answer])
+
+
+def main():
+    args = get_args()
+    questions = load_questions(args.path)
+    teach(questions)
+
+
+if __name__ == "__main__":
+    main()
