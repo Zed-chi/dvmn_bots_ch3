fa1084632595ef5cfc235e4a470dd76188f0a713
zed-chi <zed-chi@mail.ru>
2023-04-19 23:33:30 +0300

    removed logger factory

--- log_config.py
+++ log_config.py
@@ -1,19 +1,18 @@
 import logging
 
 from environs import Env
-from telegram import Bot
 
 ENV = Env()
 FORMATTER = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 
 
-def get_logger(name):
-    logger = logging.getLogger(name)
-    logger.setLevel(getattr(logging, ENV.str("LOG_LEVEL", "WARNING")))
-    return logger
-
-
 class TelegramLogsHandler(logging.Handler):
+    """
+    Due to one instance bot error
+    we need to create common bot and inject
+    it to tg runner and this handler
+    """
+
     def __init__(self, tg_bot, chat_id):
         super().__init__()
         self.chat_id = chat_id
@@ -24,17 +23,15 @@ class TelegramLogsHandler(logging.Handler):
         self.tg_bot.send_message(chat_id=self.chat_id, text=log_entry)
 
 
-def select_handler_by_env(logger: logging.Logger, bot=None):
+def get_handler_by_env(notify_bot=None):
     """options - FILE/TG/CONSOLE"""
     log_env = ENV.str("LOGGER")
     if log_env == "FILE":
-        handler = logging.FileHandler(env.str("LOG_PATH"))
-
+        handler = logging.FileHandler(ENV.str("LOG_PATH"))
     elif log_env == "TG":
-        if bot is None:
-            bot = Bot(token=ENV.str("TG_BOT_TOKEN"))
-        handler = TelegramLogsHandler(bot, ENV.str("TG_ADMIN_CHAT_ID"))
+        if not notify_bot:
+            raise ValueError("In TG Logger you need to provide a Bot instance")
+        handler = TelegramLogsHandler(notify_bot, ENV.str("TG_ADMIN_CHAT_ID"))
     else:
         handler = logging.StreamHandler()
-    handler.setFormatter(FORMATTER)
-    logger.addHandler(handler)
+    return handler
