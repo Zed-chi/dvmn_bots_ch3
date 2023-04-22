7dd0aa6cb8a5d6feb0508d56fee2d072318fb812
zed-chi <zed-chi@mail.ru>
2023-04-22 21:08:25 +0300

    changed func params

--- teach_dialogflow_from_json.py
+++ teach_dialogflow_from_json.py
@@ -32,14 +32,12 @@ def load_questions(filepath):
         return questions
 
 
-def teach(questions):
+def teach(questions, project_id):
     for intent_name, intent_data in questions.items():
         try:
             phrases = intent_data["questions"]
             answer = intent_data["answer"]
-            create_intent(
-                ENV.str("GOOGLE_CLOUD_PROJECT"), intent_name, phrases, [answer]
-            )
+            create_intent(project_id, intent_name, phrases, [answer])
         except InvalidArgument:
             logging.info(f"Раздел {intent_name} уже существует")
 
@@ -47,11 +45,13 @@ def teach(questions):
 def main():
     ENV.read_env()
     questions_path = ENV.str("QUESTIONS_JSON", None)
+    project_id = ENV.str("GOOGLE_CLOUD_PROJECT")
+
     if not questions_path:
         questions_path = get_args().path
 
     questions = load_questions(questions_path)
-    teach(questions)
+    teach(questions, project_id)
 
 
 if __name__ == "__main__":
