5285fcdf3627bd87b5687381c599ee32617fca54
zed-chi <zed-chi@mail.ru>
2023-04-25 16:04:15 +0300

    lint

--- dialogflow.py
+++ dialogflow.py
@@ -9,7 +9,9 @@ class Answer:
     text: str
 
 
-def detect_intent_texts(project_id, session_id, text, language_code):
+def detect_intent_texts(
+    project_id, session_id, text, language_code
+):
     """
     Returns the result of detect intent with texts as inputs.
     Using the same `session_id` between requests allows continuation
@@ -19,7 +21,9 @@ def detect_intent_texts(project_id, session_id, text, language_code):
     session_client = dialogflow.SessionsClient()
     session = session_client.session_path(project_id, session_id)
 
-    text_input = dialogflow.TextInput(text=text, language_code=language_code)
+    text_input = dialogflow.TextInput(
+        text=text, language_code=language_code
+    )
     query_input = dialogflow.QueryInput(text=text_input)
 
     response = session_client.detect_intent(
@@ -32,7 +36,12 @@ def detect_intent_texts(project_id, session_id, text, language_code):
     )
 
 
-def create_intent(project_id, display_name, training_phrases_parts, message_texts):
+def create_intent(
+    project_id,
+    display_name,
+    training_phrases_parts,
+    message_texts,
+):
     """
     Create an intent of the given intent type.
     """
@@ -42,9 +51,13 @@ def create_intent(project_id, display_name, training_phrases_parts, message_text
     parent = dialogflow.AgentsClient.agent_path(project_id)
     training_phrases = []
     for training_phrases_part in training_phrases_parts:
-        part = dialogflow.Intent.TrainingPhrase.Part(text=training_phrases_part)
+        part = dialogflow.Intent.TrainingPhrase.Part(
+            text=training_phrases_part
+        )
         # Here we create a new training phrase for each provided part.
-        training_phrase = dialogflow.Intent.TrainingPhrase(parts=[part])
+        training_phrase = dialogflow.Intent.TrainingPhrase(
+            parts=[part]
+        )
         training_phrases.append(training_phrase)
 
     text = dialogflow.Intent.Message.Text(text=message_texts)
