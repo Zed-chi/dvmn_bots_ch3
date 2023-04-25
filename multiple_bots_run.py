fc524da642e053732ce5483ab9d4d45e699d9426
zed-chi <zed-chi@mail.ru>
2023-04-25 16:05:49 +0300

    changed threding execution due to early app finish

--- multiple_bots_run.py
+++ multiple_bots_run.py
@@ -1,7 +1,9 @@
-import concurrent.futures as pool
+import logging
+from threading import Thread
 
 from environs import Env
 from telegram import Bot
+from telegram.utils.request import Request
 
 from log_config import get_handler_by_env
 from tg_bot import LOGGER as tg_logger
@@ -22,20 +24,31 @@ def main():
     admin_tg_chat_id = env.str("TG_ADMIN_CHAT_ID")
     vk_group_token = env.str("VK_GROUP_TOKEN")
     vk_group_id = env.str("VK_GROUP_ID")
+    google_project_name = env.str("GOOGLE_CLOUD_PROJECT")
+    log_level = env.str("LOG_LEVEL")
     logger_type = env.str("LOGGER_TYPE")
     log_filepath = env.str("LOG_PATH")
 
-    bot = Bot(token=tg_bot_token)
+    request = Request(con_pool_size=8)
+    bot = Bot(token=tg_bot_token, request=request)
     log_handler = get_handler_by_env(
         logger_type, log_filepath, bot, admin_tg_chat_id
     )
 
     tg_logger.addHandler(log_handler)
+    tg_logger.setLevel(level=getattr(logging, log_level))
     vk_logger.addHandler(log_handler)
+    vk_logger.setLevel(level=getattr(logging, log_level))
 
-    with pool.ThreadPoolExecutor() as executor:
-        executor.submit(tg, bot)
-        executor.submit(vk, vk_group_token, vk_group_id)
+    t1 = Thread(target=tg, args=[bot, google_project_name])
+    t2 = Thread(
+        target=vk,
+        args=[vk_group_token, vk_group_id, google_project_name],
+    )
+    t1.start()
+    t2.start()
+    t1.join()
+    t2.join()
 
 
 if __name__ == "__main__":
